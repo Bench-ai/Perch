f9ac35e8442e631104f220595b932c6b4cf89313
Aneesh-Pothuru <pothuruaneesh23@gmail,com>
2022-11-06 01:52:27 -0800

    [PRCH-0002], [PRCH-0003] Connected MongoDB to the Spring Boot application and created a user entity, model, repository, and controller

--- perch/src/main/java/ai/bench/perch/model/User.java
+++ perch/src/main/java/ai/bench/perch/model/User.java
@@ -1,20 +1,65 @@
-package ai.bench.test.model;
+package ai.bench.perch.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import com.mongodb.lang.NonNull;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Entity
+@Document(collection = "users")
 public class User {
 
-    private String UUID;
-    private String userName;
+    @Id
+    private String id;
 
-    public void setUUID(String uuid) {
-        this.UUID = uuid;
+    @NonNull
+    private String username;
+
+    @NonNull
+    private String email;
+
+    @NonNull
+    private String password;
+
+    public User() {
     }
 
-    @Id
-    public String getUUID() {
-        return UUID;
+    public User(@NonNull String username, @NonNull String email, @NonNull String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @NonNull
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NonNull String username) {
+        this.username = username;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
     }
 }
